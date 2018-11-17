6740bd769d646de57015726323fb4b3da75a1933
Nicole D <nnardella@gmail.com>
2018-11-17 19:56:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
 
   def match(arr)
     binding.pry
-    arr.keep_if do |str| name.split(' ').sort == str.split(' ').sort
+    arr.keep_if do |str| name.split('').sort == str.split(' ').sort
     end
   end
 
