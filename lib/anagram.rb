0421b3414e6dc9db66a35f26d19b1b1cb9407351
Nicole D <nnardella@gmail.com>
2018-11-17 19:40:13 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+  
+
+    words.select do |str|  w.include?(self.word)
+    end
+  end
+
+
+
+end
